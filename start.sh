45b92cb6fff233556d06a829b5e80cd6af044e51
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-04 16:09:29 +0100

    Test access point

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 modprobe v4l2_common && python cam-single-photo.py &
+
 cd /data
+
 python -m SimpleHTTPServer 80
+
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
 python activate_connection.py
