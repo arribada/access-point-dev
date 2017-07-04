4f3b9d490775992e2516ee299680ab5629a3172c
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-04 17:49:15 +0100

    updates

--- start.sh
+++ start.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+sleep 5
+
+ifconfig wlan1 down
+
+sleep 2
+
+ifconfig wlan1 up
+
+sleep 5
+
+python ./activate_connection.py resin-hotspot
+
 #modprobe v4l2_common && python cam-single-photo.py &
 
 cd /data
 
 python -m SimpleHTTPServer 80
-
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-
-python activate_connection.py
